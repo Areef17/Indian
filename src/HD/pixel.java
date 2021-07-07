4d18a29ff3e2db8a3f8eec1149c2fd6c2869d024
areefshaik9030@gmail.com <shaikareef9030@gmail.com>
2021-07-07 11:12:03 +0530

    this is for u

---
+++ src/HD/pixel.java
@@ -0,0 +1,11 @@
+package HD;
+
+public class pixel {
+
+	public static void main(String[] args) {
+		System.out.println("How ru buddy");
+		System.out.print("I know u");
+		System.out.println("for u budyy");
+	}
+
+}
